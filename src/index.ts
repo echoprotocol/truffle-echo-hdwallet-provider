4fdedcf79e249a14bd9118f3f96c239401e5c254
Mikhail Shautsou <m.shautsou@pixelplex.io>
2020-03-13 15:26:26 +0300

    [ET-4] Multiple accounts support

--- src/index.ts
+++ src/index.ts
@@ -2,73 +2,87 @@ import Web3 from "web3";
 import ProviderEngine from "web3-provider-engine";
 import ProviderSubprovider from "web3-provider-engine/subproviders/provider";
 import { JSONRPCRequestPayload, JSONRPCErrorCallback } from "ethereum-protocol";
-import echo, { PrivateKey, constants, Transaction } from 'echojs-lib';
+import { PrivateKey, constants, Transaction } from 'echojs-lib';
 import { parallel } from 'async';
+import { promisify } from "util";
 
 import EchoSubprovider from './EchoSubprovider';
 import Utils from './ProviderUtils';
 import { EthereumCommonTrx, EchoCommonTrxData } from './TransactionInterfaces';
 
+interface EchoProviderOptions {
+  accounts?: { [accountId: string]: PrivateKey | string };
+  syncAccountsWithTestrpc?: boolean | "always";
+  startRequestId?: number;
+}
 
 class EchoProvider {
-  public engine: ProviderEngine;
+
+  public readonly protocol: string;
+  public readonly shouldSyncAccountsWithTestrpc: boolean | "always";
+
+  public get accountsSyncedWithTestrpc() { return this._accountsSyncedWithTestrpc; }
+  private _accountsSyncedWithTestrpc = false;
+
+  private engine: ProviderEngine;
   private web3: Web3;
-  private ethAddress: string;
-  private echoId: string;
-  private privateKey: string;
-  private protocol: string;
+  private accounts: { [ethAddress: string]: PrivateKey };
+  private requestId: number;
 
-  constructor(
-    privateKey: string,
-    echoId: string,
-    web3Url: string,
-  ) {
-    this.ethAddress = Utils.idToAddress(echoId);
-    this.echoId = echoId;
-    this.privateKey = privateKey;
+  constructor(web3Url: string, options: EchoProviderOptions = {}) {
+    const accounts = options.accounts || {};
     this.protocol = web3Url.split(':')[0] || 'http';
-
     this.engine = new ProviderEngine();
     this.web3 = new Web3('ws' + web3Url.substring(web3Url.indexOf(':')));
-
-    this.web3.extend({
-      methods: [{
-        name: 'chainId',
-        call: 'eth_chainId',
-      }]
-    });
-
-    const tmpEthAddress = this.ethAddress;
-    const tmpPrivateKey = this.privateKey;
-
+    this.web3.extend({ methods: [{ name: 'chainId', call: 'eth_chainId' }] });
+    this.shouldSyncAccountsWithTestrpc = options.syncAccountsWithTestrpc || false;
+    this.requestId = options.startRequestId === undefined
+      ? Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) : options.startRequestId;
+    this.accounts = {};
+    for (const accountId in accounts) {
+      const rawPrivateKey = accounts[accountId];
+      const privateKey = typeof rawPrivateKey === "string" ? PrivateKey.fromWif(rawPrivateKey) : rawPrivateKey;
+      this.accounts[Utils.idToAddress(accountId)] = privateKey;
+    }
     this.engine.addProvider(
       new EchoSubprovider({
-        getAccounts: (cb: any) => {
-          cb(null, [tmpEthAddress]);
-        },
-        signTransaction: (txParams: EthereumCommonTrx, cb: any) => {
-          const from = txParams.from.toLowerCase();
-          if (tmpEthAddress.toLowerCase() !== from) {
-            return cb && cb('Account not found');
+        getAccounts: async (cb?: (err: unknown, accounts?: string[]) => unknown) => {
+          try {
+            await this.syncAccountsWithTestrpc();
+          } catch (error) {
+            if (cb) return cb(error);
+            throw error;
           }
-          const echoLikeTxParams = this.normalizeParams(txParams);
-          this.getExtraChainData(txParams, (err: Error | null | undefined, res: any) => {
-            if (err) return cb(err);
+          const result = Object.keys(this.accounts);
+          cb && cb(null, result);
+          return result;
+        },
+        signTransaction: async (txParams: EthereumCommonTrx, cb?: (err: unknown, res?: unknown) => unknown) => {
+          let result;
+          try {
+            await this.syncAccountsWithTestrpc();
+            const from = txParams.from.toLowerCase();
+            const privateKey: PrivateKey | undefined = this.accounts[from];
+            if (!privateKey) throw new Error("Account not found");
+            const echoLikeTxParams = this.normalizeParams(txParams);
+            const res: any = await promisify(this.getExtraChainData.bind(this))(txParams);
             const fee = res.gas;
             const chainIdUnhandle = res.id;
             const chainId = chainIdUnhandle.slice(2);
             const head_block_number = res.block.number.toString();
             const head_block_id = res.block.hash.slice(26);
-
-            echoLikeTxParams[1].fee = {
-              asset_id: '1.3.0', amount: fee
-            }
+            echoLikeTxParams[1].fee = { asset_id: '1.3.0', amount: fee };
             const tx: any = new Transaction().addOperation(...echoLikeTxParams);
             tx.chainId = chainId;
             tx.refBlockNum = Number(head_block_number);
             tx.refBlockPrefix = head_block_id;
-            this.serializeTransaction(tx, tmpPrivateKey, cb)
-          });
+            result = await promisify(this.serializeTransaction.bind(this))(tx, privateKey);
+          } catch (error) {
+            if (cb) return cb(error);
+            throw error;
+          }
+          if (cb) cb(null, result);
+          return result;
         }
       })
     );
@@ -89,8 +103,28 @@ class EchoProvider {
     this.engine.start();
   }
 
-  private serializeTransaction(tx: any, tmpPrivateKey: string, cb: any) {
-    tx.sign(PrivateKey.fromWif(tmpPrivateKey)).then(() => {
+  public async syncAccountsWithTestrpc(): Promise<void> {
+    if (
+      !this.shouldSyncAccountsWithTestrpc ||
+      (this.accountsSyncedWithTestrpc && this.shouldSyncAccountsWithTestrpc !== "always")
+    ) return;
+    const accounts: { address: string; privkey: string }[] = await promisify(this.send.bind(this))({
+      jsonrpc: "2.0",
+      id: this.requestId++,
+      method: "personal_listRawAccounts",
+      params: [],
+    }).then((res) => {
+      if (res === undefined) throw new Error("personal_listRawAccounts not returns data");
+      return res.result;
+    });
+    for (const { address, privkey } of accounts) {
+      this.accounts[address] = PrivateKey.fromBuffer(Buffer.from(privkey.slice(2), "hex"));
+    }
+  }
+
+  private serializeTransaction(tx: any, rawPrivateKey: string | PrivateKey, cb: any) {
+    const privateKey = typeof rawPrivateKey === "string" ? PrivateKey.fromWif(rawPrivateKey) : rawPrivateKey;
+    tx.sign(privateKey).then(() => {
       const rawTx = tx.signedTransactionSerializer().toString('hex');
       cb(null, rawTx);
     });
@@ -113,10 +147,11 @@ class EchoProvider {
   }
 
   private normalizeParams(txParams: EthereumCommonTrx): EchoCommonTrxData {
+    const from = Utils.addressToId(txParams.from);
     if (txParams.to && txParams.data) {
       const contractId = Utils.addressToId(txParams.to);
       return [constants.OPERATIONS_IDS.CONTRACT_CALL, {
-        registrar: this.echoId,
+        registrar: from,
         value: {
           asset_id: '1.3.0',
           amount: txParams.value || 0,
@@ -127,7 +162,7 @@ class EchoProvider {
       }];
     } else if (txParams.data && !txParams.to) {
       return [constants.OPERATIONS_IDS.CONTRACT_CREATE, {
-        registrar: this.echoId,
+        registrar: from,
         value: {
           asset_id: '1.3.0',
           amount: 0
@@ -138,7 +173,7 @@ class EchoProvider {
     } else if (!txParams.data && txParams.to) {
       const receiverId = Utils.addressToId(txParams.to);
       return [constants.OPERATIONS_IDS.TRANSFER, {
-        from: this.echoId,
+        from,
         to: receiverId,
         amount: {
           asset_id: '1.3.0',
